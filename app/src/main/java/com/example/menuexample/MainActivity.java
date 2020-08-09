99dbe88f5b1216eab6d1d84dc7d2f3a9dc2d5ca5
PhyoeKo <developer.phyoeko@gmail.com>
2020-08-09 13:54:32 +0630

    time picker added

--- app/src/main/java/com/example/menuexample/MainActivity.java
+++ app/src/main/java/com/example/menuexample/MainActivity.java
@@ -23,7 +23,8 @@ import androidx.fragment.app.FragmentActivity;
 
 public class MainActivity extends FragmentActivity implements DatePickerFragment.DatePickedListener {
 
-    TextView dateView, timeView;
+    TextView dateView;
+    static TextView timeView;
     String date;
 
     @Override
@@ -51,4 +52,27 @@ public class MainActivity extends FragmentActivity implements DatePickerFragment
     public void onDatePicked(String date) {
 
     }
+
+
+    public static class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            timeView.setText(hourOfDay%12 + ":" + minute + " " + ((hourOfDay>=12) ? "PM" : "AM"));
+        }
+    }
 }
+
+
